3a082a9891ea5766b7eafab8049f3ae7ce1eeb21
MuriloMelo-Dev <murilo.melo@inovanti.com.br>
2025-09-02 09:41:29 -0300

    style: remove unnecessary exports

--- src/Messaging/Providers/SendGridProvider.php
+++ src/Messaging/Providers/SendGridProvider.php
@@ -2,9 +2,7 @@
 
 namespace InovantiBank\Messaging\Providers;
 
-use App\Validators\AttachmentValidator;
 use Exception;
-use Illuminate\Support\Facades\Log;
 use InovantiBank\Messaging\Contracts\MessagingProviderInterface;
 use InovantiBank\Messaging\DTOs\MessageData;
 use SendGrid;
